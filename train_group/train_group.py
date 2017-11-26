49cba39c5dcb34ab3f9e088d376412b27cfc6010
Clemens Hutter <shakti@Clemenss-MacBook-Air.local>
2017-11-26 09:06:26 +0000

    training bot

---
+++ train_group/train_group.py
@@ -0,0 +1,105 @@
+import requests
+import json
+
+global config
+global ms_id_2_key
+global key_2_ms_id
+
+ms_id_2_key = dict()
+key_2_ms_id = dict()
+
+with open("people.json", "r") as f:
+    config = json.load(f)
+
+subscription_key = '7a9c77da2eb34dd5b13d9dda1ca00071'
+uri_base = 'westeurope.api.cognitive.microsoft.com'
+personGroupID = config["personGroupID"]
+
+
+def add_person(personID):
+    headers = {
+        # Request headers
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': subscription_key,
+    }
+
+    body = {
+        "name": personID,
+        "userData": personID,
+    }
+
+    response = requests.request('POST', 'https://' + uri_base + '/face/v1.0/persongroups/{}/persons'.format(personGroupID), headers=headers, json=body)
+    r = json.loads(response.text)
+    print(r)
+    ms_personID = r['personId']
+
+    ms_id_2_key[ms_personID] = personID
+    key_2_ms_id[personID] = ms_personID
+
+
+
+
+def add_face(personID, picID):
+    ms_personID = key_2_ms_id[personID]
+
+    # Request headers.
+    headers = {
+        'Content-Type': 'application/octet-stream',
+        'Ocp-Apim-Subscription-Key': subscription_key,
+    }
+
+    fp = open("{}/{}".format(personID, picID), "rb")
+
+    # Request parameters.
+    params = {
+        'userData': personID + "_" + picID,
+    }
+
+    # Body. The URL of a JPEG image to analyze.
+
+    # Execute the REST API call and get the response.
+    response = requests.request('POST', 'https://' + uri_base + '/face/v1.0/persongroups/{}/persons/{}/persistedFaces'.format(personGroupID, ms_personID), data=fp, headers=headers, params=params)
+
+    print(response)
+    print(response.text)
+
+def train_group():
+    print("training")
+    headers = {
+        # Request headers
+        'Ocp-Apim-Subscription-Key': subscription_key,
+    }
+
+    response = requests.request('POST', 'https://' + uri_base + '/face/v1.0/persongroups/{}/train'.format(personGroupID), headers=headers)
+    print(response)
+    print(response.text)
+
+def add_group():
+    headers = {
+        # Request headers
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': subscription_key,
+    }
+    body = {
+        'name': personGroupID,
+    }
+
+    response = requests.request('PUT', 'https://' + uri_base + '/face/v1.0/persongroups/{}'.format(personGroupID), json=body, headers=headers)
+    print(response)
+    print(response.text)
+
+
+
+def train_all_faces():
+    add_group()
+    people = config["people"]
+    pics = config["files"]
+    for pers in people:
+        add_person(pers)
+        for pic in pics:
+            add_face(pers, pic)
+
+    train_group()
+
+
+train_all_faces()
